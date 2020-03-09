f9b65764f7aa37aae52790b1cf223b89c992292c
FlorianSdorra <fsd114@posteo.de>
2020-03-09 15:52:25 +0100

    here just the puzzlepieces / got rid of the redux / just here for being copied

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+import LandingPage from './components/LandingPage';
+import SearchResult from './components/SearchResult';
+function App() {
+  return (
+    <div className="App">
+        <LandingPage></LandingPage>
+        <div className="result-list-wrap">
+                <SearchResult></SearchResult>
+                <SearchResult></SearchResult>
+                <SearchResult></SearchResult>
+                <SearchResult></SearchResult>
+                <SearchResult></SearchResult>
+                <SearchResult></SearchResult>
+          </div>
+      </div>
+  );
+}
+
+export default App;
