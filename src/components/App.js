af339cb441e3dd0c5103d5cbfe62082fd02547f0
casinca4 <s.antonelli@web.de>
2020-03-10 10:54:18 +0100

    Initial commit. This will add basic structure to create header and footer.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../scss/App.scss";
-import FullPage from "./fullPage";
+// import FullPage from "./fullPage";
 import { connect } from "react-redux";
+import logo from '../assets/logo.png';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <FullPage></FullPage>
+        <header>
+          <div className="logo"><img src={logo} alt="logo hey dj"></img></div>
+          <button className="login-button">Login</button>
+        </header>
+        
+        <footer>
+          <p>Contact &amp; Imprint</p>
+        </footer>
       </div>
     );
   }
