0b569b9a1c9ec87cf4b915e85394c4248cf16e76
florianbux <florian.bux@googlemail.com>
2020-03-10 15:58:11 +0100

    This will add needed Fullpage feature.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 import React from "react";
 import "../scss/App.scss";
 // import FullPage from "./fullPage";
+import ReactFullpage from "@fullpage/react-fullpage";
 import { connect } from "react-redux";
 
 class App extends React.Component {
     render() {
-        return <div className="App"></div>;
+        return (
+            <div className="App">
+                <ReactFullpage
+                    //fullpage options
+                    licenseKey={"YOUR_KEY_HERE"}
+                    scrollingSpeed={1000}
+                    navigation
+                    navigationPosition={"left"} /* Options here */
+                    render={({ state, fullpageApi }) => {
+                        return (
+                            <ReactFullpage.Wrapper>
+                                <div className="section">
+                                    <p>Section 1 (welcome to fullpage.js)</p>
+                                    <button
+                                        onClick={() =>
+                                            fullpageApi.moveSectionDown()
+                                        }
+                                    >
+                                        Click me to move down
+                                    </button>
+                                </div>
+                                <div className="section">
+                                    <p>Section 2</p>
+                                </div>
+                            </ReactFullpage.Wrapper>
+                        );
+                    }}
+                />
+            </div>
+        );
     }
 }
 
--- src/components/fullPage.js
+++ src/components/fullPage.js
@@ -2,18 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 
 class FullPage extends React.Component {
-  render() {
-    return (
-      <div className="main-container">
-        <div className="fullpage"></div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="main-container">
+                <div className="fullpage"></div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return state;
+    return state;
 };
 
-export default connect(mapStateToProps, {})(FullPage)
-
+export default connect(mapStateToProps, {})(FullPage);
