e596e0d58beba6398cbc81ddddc7937709c658fc
florianbux <florian.bux@googlemail.com>
2020-03-11 14:54:02 +0100

    This adds the onclick & fixes and adjustments.

--- src/components/fullPage.js
+++ src/components/fullPage.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { connect } from "react-redux";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 class FullPage extends React.Component {
     render() {
         return (
-            <div className="main-container">
-                <div className="fullpage"></div>
-            </div>
+            <ReactFullpage
+                //fullpage options
+                licenseKey={"YOUR_KEY_HERE"}
+                scrollingSpeed={1000}
+                loopBottom={true}
+                navigation
+                navigationPosition={"left"} /* Options here */
+                render={({ state, fullpageApi }) => {
+                    return (
+                        <ReactFullpage.Wrapper>
+                            <div className="section">
+                                <p>Section 1 (welcome to fullpage.js)</p>
+                                {/* <button
+                                    onClick={() =>
+                                        fullpageApi.moveSectionDown()
+                                    }
+                                >
+                                    Click me to move down
+                                </button> */}
+                                <div
+                                    className="center"
+                                    onClick={() =>
+                                        fullpageApi.moveSectionDown()
+                                    }
+                                >
+                                    <button>About</button>
+                                    <div className="arrow">
+                                        <i className="bt-down"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section">
+                                <p>Section 2</p>
+                            </div>
+                        </ReactFullpage.Wrapper>
+                    );
+                }}
+            />
         );
     }
 }
