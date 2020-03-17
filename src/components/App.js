78269a966c6e617f69238b5a1308124275818a5c
casinca4 <s.antonelli@web.de>
2020-03-17 16:58:41 +0100

    Add banner and event description

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,37 @@ import "../scss/App.scss";
 // import FullPage from "./fullPage";
 import { connect } from "react-redux";
 import logo from '../assets/logo.png';
+import banner from '../assets/banner.png';
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-          <header>
-            <div className="logo-heydj">
-              <img src={logo} alt="logo hey dj"></img>
-            </div>
-            <div className="header-right-wrap">
-              {/* <button className="login-button">Login</button> */}
-              {/* <i className="exit fas fa-times"></i> */}
-              <i className="settings fas fa-cog"></i> 
-            </div>
-          </header>
+        <header>
+          <div className="logo-heydj">
+            <img src={logo} alt="logo hey dj"></img>
+          </div>
+          <div className="header-right-wrap">
+            {/* <button className="login-button">Login</button> */}
+            {/* <i className="exit fas fa-times"></i> */}
+            <i className="settings fas fa-cog"></i>
+          </div>
+        </header>
         <main>
-          
+          <div className="banner">
+            <img src={banner}></img>
+          </div>
+          <section className="description-event">
+            <p>Fvck Genre, King Kong Kicks, 27.3.3030, Musik und Frieden </p>
+            <p>Everything your heart desires.</p>
+            <br></br>
+            <h1>Add your song to the request list.</h1>
+          </section>
         </main>
         <footer>
           <div>
-          <a href="#">Contact &amp; Imprint</a>
+            <a href="#">Contact &amp; Imprint</a>
           </div>
         </footer>
       </div>
@@ -36,3 +46,4 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {})(App);
+
