90e13fcfd2d7c78fbcbf441c0c7cdf124a614db4
florianbux <florian.bux@googlemail.com>
2020-03-31 16:51:55 +0200

    This will add EventSearchResult component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "normalize.css"; // This adds the normalize.css !
 import "../scss/App.scss";
 
-import FullPage from "./LandingPage";
+import EventSearchResult from "./EventSearchResult";
 // import ReactFullpage from "@fullpage/react-fullpage";
 import { connect } from "react-redux";
 
@@ -10,7 +10,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <FullPage />
+                <EventSearchResult />
             </div>
         );
     }
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-class SearchResult extends React.Component{
-    render(){
-        return(
+class SearchResult extends React.Component {
+    render() {
+        return (
             <div className="result-list-item">
-            <div className="result-image-container">
-                <div className="result-image"></div>
+                <div className="result-image-container">
+                    <div className="result-image"></div>
+                </div>
+                <div className="result-info-container">
+                    <p>This is a result</p>
+                    <hr />
+                    <p>Some info</p>
+                </div>
             </div>
-            <div className="result-info-container">
-                <p>This is a result</p>
-                <hr/>
-                <p>Some info</p>
-            </div>
-        </div>
-        )
+        );
     }
 }
 
