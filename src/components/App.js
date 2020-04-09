55cb66dfbf618937e87f1da9fb375f3d9f5d3561
FlorianSdorra <fsd114@posteo.de>
2020-04-09 10:27:03 +0200

    simple routing for now / kinda preparing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "normalize.css"; // This adds the normalize.css !
 import "../scss/App.scss";
 
-import FullPage from "./LandingPage";
+import LandingPage from "./LandingPage";
 import EventSearchResult from "./EventSearchResult";
 import EventOverview from "./Event";
 import TrackSearch from "./TrackSearch";
@@ -12,12 +14,16 @@ import { connect } from "react-redux";
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                {/* <FullPage /> */}
-                {/* <EventSearchResult /> */}
-                {/* <EventOverview /> */}
-                <TrackSearch />
-            </div>
+            <Router>
+                <Switch>
+                    <div className="App">
+                        <Route exact path="/" component={LandingPage} /> 
+                        <Route path="/eventsearch" component={EventSearchResult} />
+                        <Route path="/eventoverview" component={EventOverview} /> 
+                        <Route path="tracksearch" component={TrackSearch} />
+                    </div>
+                </Switch>
+            </Router>
         );
     }
 }
