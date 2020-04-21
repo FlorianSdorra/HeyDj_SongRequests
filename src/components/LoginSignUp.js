e063b6ef3aff68310f63d7c62cd2e91c7b257103
florianbux <florian.bux@googlemail.com>
2020-04-21 11:06:50 +0200

    ready to merge!

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import ReactFullpage from "@fullpage/react-fullpage";
 
 import logo from "../assets/logo.png";
-// import LandingPage from "./LandingPage";
 
 class LoginSignUp extends React.Component {
     render() {
